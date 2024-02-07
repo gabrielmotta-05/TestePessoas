146914b234cf714b4da642e26d8e66d2ec2b5041
Gabriel <gabriel.motta05@gmail.com>
2024-02-07 10:07:59 -0300

    criação tabela pessoa

---
+++ TestePessoa/20240207CREATETABLEPESSOA.js
@@ -0,0 +1,8 @@
+﻿//Script para criar a tabela de PessoaFisica
+CREATE TABLE Pessoa(
+    Id INT PRIMARY KEY IDENTITY(1, 1),
+    NomeCompleto NVARCHAR(100) NOT NULL,
+    DataNascimento DATE NOT NULL,
+    ValorRenda DECIMAL(10, 2) NOT NULL,
+    CPF NVARCHAR(11) NOT NULL UNIQUE
+);
